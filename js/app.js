c6ca50bad0dc49329d34fe9c49701e8ad7496abb
Irina Kuznetsova <irinakuznetsova@Irinas-MacBook-Pro.local>
2016-08-24 20:35:19 +0300

    Getting tasks from json file, simplified sructure

--- js/app.js
+++ js/app.js
@@ -6,15 +6,66 @@
 
   // Controllers section -----------------------------------------------------
 
-  app.controller('TimerController', function(){
+  app.controller('TimerController', function($scope, $timeout){
 
-    this.start_btn = start;
-    this.stop_btn = stop;
-    this.skip_btn = skip;
+    console.log($scope);
+
+    $scope.start_btn = 'Start';
+    $scope.stop_btn = 'Stop';
+    $scope.skip_btn = 'Skip';
+
+    $scope.seconds = 0;
+    $scope.minutes = 0;
+    $scope.time = 0;
+
+    // Methods for timer buttons ---------------------------------------------
+
+    var timer;
+
+    $scope.start_timer = function(){
+
+        $scope.seconds += 1;
+        $scope.time += 1;
+
+        timer = $timeout($scope.start_timer, 500);
+        $scope.minutes = Math.floor($scope.time / 60);
+
+        if ($scope.seconds == 60){$scope.seconds = 0}
+
+    };
+
+    $scope.skip_timer = function(){
+        $scope.seconds = 0;
+        $scope.start_timer
+    };
+
+    $scope.stop_timer = function(){
+
+        $timeout.cancel(timer)
+
+    };
+
+    // Function - flag if preceding zero should should be shown --------------
+
+    $scope.is_zero_shown = function(){
+
+       // Preceding zero for minutes is hiding with preceding zero for seconds
+       // Parameter for function is needed
+       if (($scope.seconds > 9) || ($scope.minutes > 9))
+          return true;
+        else
+          return false;
+
+    };
 
   });
 
-  app.controller('MenuController', function(){
+  app.controller('MenuController', function($scope){
+
+    $scope.app_name = 'Mimidoro';
+    $scope.tasks_btn = 'Tasks';
+    $scope.reports_btn = 'Reports';
+    $scope.settings_btn = 'Settings';
 
     this.tab = 1;
 
@@ -30,80 +81,58 @@
 
   });
 
-  app.controller('TasksController', function(){
+  app.controller('TasksController', ['$scope', '$http', '$log', function($scope, $http, $log){
 
-    this.task = {};
+    $scope.tasks_list = [];
+    $scope.add_task_btn = 'Add Task';
 
-    this.addTask = function(task){
-        tasks.push(this.task);
-        this.task = {};
+    $scope.task = {};
+
+    $scope.addTask = function(task){
+        $scope.tasks_list.push($scope.task);
+
+
+        $http.post('tasks.json', {'description': $scope.task})
+        .then(function (response) {
+            return response;
+        });
+
+        $scope.task = {};
     };
 
-    this.list = tasks;
-    this.add_task = add;
+    $http.get('tasks.json').success(function(data){
+
+        $scope.tasks_list = data;
+
+        });
+
+  }]);
+
+  app.controller('ReportsController', function($scope){
+
+    $scope.reports = 'Reports - will be implemented';
 
   });
 
-  app.controller('ReportsController', function(){
+  app.controller('SettingsController', function($scope){
 
-    this.reports_list = reports;
-
-  });
-
-  app.controller('SettingsController', function(){
-
-    this.settings_list = settings;
+    $scope.settings = 'Settings - will be implemented';
 
   });
 
   // Variables section -------------------------------------------------------
 
-  var start = {
-
-    caption: 'Start'
-
-  }
-
-   var stop = {
-
-    caption: 'Stop'
-
-  }
-
-   var skip = {
-
-    caption: 'Skip'
-
-  }
-
-  var add = {
-
-    caption: 'Add Task'
-
-  }
-
-  var reports = {
-
-    report_name: 'Reports - will be implemented'
-
-  }
-
-  var settings = {
-
-    setting_name: 'Settings - will be implemented'
-  }
-
-  var tasks = [{
-    title: 'Task 1',
-    description: 'Finish course "Shaping Up with AngularJS" on CodeSchool'
-  },
-  {
-    title: 'Task 2',
-    description: 'Implement on click action for "Start" button -> Timer starts'
-  },
-  {
-    title: 'Task 3',
-    description: 'Entering value into task field appends new task into the list'
-  }];
+//  var tasks = [{
+//    title: 'Task 1',
+//    description: 'Finish course "Shaping Up with AngularJS" on CodeSchool'
+//  },
+//  {
+//    title: 'Task 2',
+//    description: 'Implement on click action for "Start" button -> Timer starts'
+//  },
+//  {
+//    title: 'Task 3',
+//    description: 'Entering value into task field appends new task into the list'
+//  }];
 
 })();
